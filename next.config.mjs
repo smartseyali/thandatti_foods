55f0a36f421333cb683087cc508cc31cd799c464
vigneshramraj <153055884+vigneshramraj@users.noreply.github.com>
2025-12-09 09:41:36 +0530

    Standalone app

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
     // Uncomment when add value for NEXT_PUBLIC_PATH in .env.production or .env.development
     // basePath: process.env.NEXT_PUBLIC_PATH,
     trailingSlash: true,
